d2bc401cd33b5d96c87fa297d998437368a8ed1c
hf-zhaozhiming <729144668@qq.com>
2017-04-17 18:04:34 +0800

    cc

---
+++ src/cn/map02/TreeMapDemo01.java
@@ -0,0 +1,29 @@
+package cn;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class TreeMapDemo01 {
+	public static void main(String[] args) {
+		TreeMap<Student, String> hm = new TreeMap<Student, String>();
+		Student s1 = new Student("草泥马", 11);
+		Student s2 = new Student("赵云", 12);
+		Student s3 = new Student("李四", 13);
+		Student s4 = new Student("王五", 53);
+		Student s5 = new Student("草泥马", 11);
+
+		hm.put(s1, "s1");
+		hm.put(s2, "s2");
+		hm.put(s3, "s3");
+		hm.put(s4, "s4");
+		hm.put(s5, "s5");
+
+		Set<Student> set = hm.keySet();
+		for (Student s : set) {
+			String value = hm.get(s);
+			System.out.println(s.getName() + "----" + s.getAge() + "----"
+					+ value);
+		}
+
+	}
+}
