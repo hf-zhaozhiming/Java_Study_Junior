f7d8d097b64b886c59508129e11a5de77fe14c11
hf-zhaozhiming <729144668@qq.com>
2017-04-26 18:21:32 +0800

    xccc

--- src/cn/IO01/IODemo05.java
+++ src/cn/IO01/IODemo05.java
@@ -1,5 +1,22 @@
 package cn.IO01;
 
-public class IODemo05 {
+import java.io.File;
 
+public class IODemo05 {
+	public static void main(String[] args) {
+		File file = new File("d:\\360Downloads");
+		findJPG(file);
+	}
+
+	private static void findJPG(File file) {
+		File[] fileArray = file.listFiles();
+		for (File f : fileArray) {
+			if (!f.isFile()) {
+				// System.out.println(f.getAbsolutePath());
+				findJPG(f);
+			} else if (f.getName().endsWith(".jpg")) {
+				System.out.println(f.getAbsolutePath());
+			}
+		}
+	}
 }
